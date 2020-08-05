75c81269814f225832760df2704bd1b405ac12e8
Shaharkl <sk2601kl@gmail.com>
2020-08-05 13:57:42 +0300

    amsma

--- pipeline.py
+++ pipeline.py
@@ -4,19 +4,20 @@ import docker
 
 username = "shaharkl/"
 reponame = "advance_pipeline"
-version = "v0.0.0"
 client = docker.from_env()
 
 parser = argparse.ArgumentParser(description='Process some variables.')
 parser.add_argument('-m', metavar='m', type=str, help='an msg for git commit')
 parser.add_argument('-f', metavar='f',  nargs='+', type=str, help='the args for git add')
-parser.add_argument('-v', metavar='v', type=str, help='the version for image name')
+#parser.add_argument('-v', metavar='v', type=str, help='the version for image name')
 args = parser.parse_args()
 
 
 for element in args.f:
     subprocess.call(['git', 'add', element])
 subprocess.call(["git", "commit", '-m', args.m])
+version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
+print(version)
 subprocess.call(["git", "push"])
 
 subprocess.call(['docker', 'build', '.', '-t', username+reponame+':'+version])
