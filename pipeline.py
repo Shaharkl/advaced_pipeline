41e95b3b267a8c611a2f11ef6ceff4a21e8ac0e0
Shaharkl <sk2601kl@gmail.com>
2020-08-05 11:50:03 +0300

    amsma

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,23 @@
-from git import Repo, remote
 import argparse
 import subprocess
+import docker
+
+username = "Shaharkl"
+reponame = "advance_pipeline"
+version = "v0.0.0"
+client = docker.from_env()
 
 parser = argparse.ArgumentParser(description='Process some variables.')
 parser.add_argument('-m', metavar='m', type=str, help='an msg for git commit')
 parser.add_argument('-f', metavar='f',  nargs='+', type=str, help='the args for git add')
 parser.add_argument('-v', metavar='v', type=str, help='the version for image name')
 args = parser.parse_args()
-print(args.f[0])
+
 
 for element in args.f:
     subprocess.call(['git', 'add', element])
 subprocess.call(["git", "commit", '-m', args.m])
 subprocess.call(["git", "push"])
+
+client.images.build(path = "./", tag= reponame + ":" + version)
 
