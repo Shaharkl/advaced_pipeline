df78e7c4b501ba0b64dc964dc3f2a884d0a82e68
Shaharkl <sk2601kl@gmail.com>
2020-08-10 17:04:29 +0300

    new update

--- pipeline.py
+++ pipeline.py
@@ -9,12 +9,11 @@ client = docker.from_env()
 parser = argparse.ArgumentParser(description='Process some variables.')
 parser.add_argument('-m', metavar='m', type=str, help='an msg for git commit')
 parser.add_argument('-f', metavar='f',  nargs='+', type=str, help='the args for git add')
-#parser.add_argument('-v', metavar='v', type=str, help='the version for image name')
 args = parser.parse_args()
 
 
 for element in args.f:
-    subprocess.call(['git', 'add', element])
+    subprocess.call(['git', 'add', element])       
 subprocess.call(["git", "commit", '-m', args.m])
 process = subprocess.Popen(['git', 'rev-parse', 'HEAD'], shell=False, stdout=subprocess.PIPE)
 git_head_hash = process.communicate()[0].strip()
